0d09ef5834e422c36428eda27dbed1880aa64a1d
Ognyan Bankov <ogibankov@gmail.com>
2022-09-08 18:22:22 +0300

    removing createHtmlResponse methods

--- src/main/kotlin/com/bolyartech/forge/server/handler/WebPage.kt
+++ src/main/kotlin/com/bolyartech/forge/server/handler/WebPage.kt
@@ -2,11 +2,9 @@ package com.bolyartech.forge.server.handler
 
 import com.bolyartech.forge.server.misc.TemplateEngine
 import com.bolyartech.forge.server.misc.TemplateEngineFactory
-import com.bolyartech.forge.server.response.HtmlResponse
 import com.bolyartech.forge.server.response.Response
 import com.bolyartech.forge.server.response.ResponseException
 import com.bolyartech.forge.server.route.RequestContext
-import jakarta.servlet.http.Cookie
 
 /**
  * Handler which provides normal web page functionality.
@@ -30,26 +28,5 @@ abstract class WebPage(
 
     final override fun handle(ctx: RequestContext): Response {
         return handlePage(ctx, templateEngineFactory.createNew())
-    }
-
-    /**
-     * Convenience method for creating HtmlResponse
-     *
-     * @param content
-     * @return
-     */
-    final fun createHtmlResponse(content: String): HtmlResponse {
-        return HtmlResponse(content, enableGzipSupport)
-    }
-
-    /**
-     * Convenience method for creating HtmlResponse
-     *
-     * @param content
-     * @param cookiesToSet
-     * @return
-     */
-    fun createHtmlResponse(content: String, cookiesToSet: List<Cookie>): HtmlResponse {
-        return HtmlResponse(cookiesToSet, content, enableGzipSupport)
     }
 }
