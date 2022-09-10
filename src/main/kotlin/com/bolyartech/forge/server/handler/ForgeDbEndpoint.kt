14724f4f4d822f0c56ec5d828eed4baf22d76c2a
Ognyan Bankov <ogibankov@gmail.com>
2022-09-10 10:07:30 +0300

    adding file

---
+++ src/main/kotlin/com/bolyartech/forge/server/handler/ForgeDbEndpoint.kt
@@ -0,0 +1,24 @@
+package com.bolyartech.forge.server.handler
+
+import com.bolyartech.forge.server.db.DbPool
+import com.bolyartech.forge.server.response.ResponseException
+import com.bolyartech.forge.server.response.forge.ForgeResponse
+import com.bolyartech.forge.server.route.RequestContext
+import java.sql.Connection
+import java.sql.SQLException
+
+abstract class ForgeDbEndpoint(private val dbPool: DbPool) : ForgeEndpoint() {
+    abstract fun handleForge(ctx: RequestContext, dbc: Connection) : ForgeResponse
+
+    final override fun handleForge(ctx: RequestContext): ForgeResponse {
+        try {
+            dbPool.connection.use { dbc ->
+                val ret: ForgeResponse = handleForge(ctx, dbc)
+                dbc.close()
+                return ret
+            }
+        } catch (e: SQLException) {
+            throw ResponseException(e)
+        }
+    }
+}
